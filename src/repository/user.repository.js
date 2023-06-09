387bfc0c940b131cdc510e038e163b32aec885f8
mrkrimers <mrkrimerssasha@gmail.com>
2023-06-09 16:42:07 +0300

    added patch prettier eslint

--- src/repository/user.repository.js
+++ src/repository/user.repository.js
@@ -1,77 +1,77 @@
 const pool = require(`../bd`);
 
 async function getAllUsersDB() {
-  const client = await pool.connect();
+    const client = await pool.connect();
 
-  const sql = `SELECT * FROM users`;
+    const sql = `SELECT * FROM users`;
 
-  const data = (await client.query(sql)).rows;
+    const data = (await client.query(sql)).rows;
 
-  return data;
+    return data;
 }
 
 async function postUsersCreateDB(name, surname, email, pwd) {
-  const client = await pool.connect();
+    const client = await pool.connect();
 
-  const sql = `INSERT INTO users (name, surname, email, pwd)
+    const sql = `INSERT INTO users (name, surname, email, pwd)
     VALUES ($1, $2, $3, $4)
-    RETURNING *`;
+    RETURNING *`
 
-  const data = (await client.query(sql, [name, surname, email, pwd])).rows;
+    const data = (await client.query(sql, [name, surname, email, pwd])).rows;
 
-  return data;
+    return data;
 }
 
 async function getUserByIdDB(id) {
-  const client = await pool.connect();
+    const client = await pool.connect();
 
-  const sql = `SELECT * FROM users WHERE id = $1`;
+    const sql = `SELECT * FROM users WHERE id = $1`;
 
-  const data = (await client.query(sql, [id])).rows;
+    const data = (await client.query(sql, [id])).rows;
 
-  return data;
+    return data;
 }
 
 async function putUserUpdateDB(id, name, surname, email, pwd) {
-  const client = await pool.connect();
+    const client = await pool.connect();
 
-  const sql = `UPDATE users 
+    const sql = `UPDATE users 
     SET name = $2, surname = $3, email = $4, pwd = $5 
     WHERE id = $1
     RETURNING *`;
 
-  const data = (await client.query(sql, [id, name, surname, email, pwd])).rows;
+    const data = (await client.query(sql, [id, name, surname, email, pwd])).rows;
 
-  return data;
+    return data;
 }
 
 async function deleteUserDB(id) {
-  const client = await pool.connect();
+    const client = await pool.connect();
 
-  const sql = `DELETE FROM users 
+    const sql = `DELETE FROM users 
     WHERE id = $1 
     RETURNING *`;
 
-  const data = (await client.query(sql, [id])).rows;
+    const data = (await client.query(sql, [id])).rows;
 
-  return data;
+    return data;
 }
 
 async function patchUsersDB(id, clientData) {
-  const client = await pool.connect();
+    const client = await pool.connect();
 
-  const firstSql = `SELECT * FROM users WHERE id = $1`;
-  const fromDB = (await client.query(firstSql, [id])).rows;
+    const firstSql = `SELECT * FROM users WHERE id = $1`;
+    const fromDB = (await client.query(firstSql, [id])).rows;
 
-  const patchedData = { ...fromDB[0], ...clientData };
+    const patchedData = { ...fromDB[0], ...clientData };
 
-  const secondSql = `UPDATE users
+    const secondSql = `UPDATE users
     SET name = $2, surname = $3, email = $4, pwd = $5 
     WHERE id = $1
     RETURNING *`;
-  const dataUpdate = (await client.query(secondSql, [id, patchedData.name, patchedData.surname, patchedData.email, patchedData.pwd])).rows;
+    const dataUpdate = (await client.query(secondSql, [id, patchedData.name, patchedData.surname, patchedData.email, patchedData.pwd])).rows;
 
-  return dataUpdate;
+    return dataUpdate;
 }
 
 module.exports = { getAllUsersDB, postUsersCreateDB, getUserByIdDB, putUserUpdateDB, deleteUserDB, patchUsersDB };
