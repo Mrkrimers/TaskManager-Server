1bb04037c1dd566904fb9fec70debceea1afd3d1
mrkrimers <mrkrimerssasha@gmail.com>
2023-06-12 21:16:37 +0300

    added Exceptions user

--- src/service/user.service.js
+++ src/service/user.service.js
@@ -1,43 +1,44 @@
 const { getAllUsersDB, postUsersCreateDB, getUserByIdDB, putUserUpdateDB, deleteUserDB, patchUsersDB } = require(`../repository/user.repository`);
+const ExceptionType = require(`../exceptions/exceptions`)
 
 async function getAllUsers() {
   const data = await getAllUsersDB();
-  if (!data.length) throw new Error(`DB is EMPTY`);
+  if (!data.length) throw new Error(ExceptionType.DB_GET_USERS_NOT_FOUND);
 
   return data;
 }
 
 async function getUserById(id) {
   const data = await getUserByIdDB(id);
-  if (!data) throw new Error(`EMPRY`);
+  if (!data) throw new Error(ExceptionType.DB_GET_USER_NOT_FOUND);
 
   return data;
 }
 
 async function postUsersCreate(name, surname, email, pwd) {
   const data = await postUsersCreateDB(name, surname, email, pwd);
-  if (!data) throw new Error(`EMPRY`);
+  if (!data) throw new Error(ExceptionType.DB_POST_USER_NOT_CREATED);
 
   return data;
 }
 
 async function putUserUpdate(id, name, surname, email, pwd) {
   const data = await putUserUpdateDB(id, name, surname, email, pwd);
-  if (!data.length) throw new Error(`not Created`);
+  if (!data.length) throw new Error(ExceptionType.DB_PUT_USER_NOT_UPDATED);
 
   return data;
 }
 
 async function deleteUser(id) {
   const data = await deleteUserDB(id);
-  if (!data.length) throw new Error(`ID not found`);
+  if (!data.length) throw new Error(ExceptionType.DB_DELETE_USER_NOT_DELETED);
 
   return data;
 }
 
 async function patchUsers(id, clientData) {
   const data = await patchUsersDB(id, clientData);
-  if (!data.length) throw new Error(`ERROR!!!!`);
+  if (!data.length) throw new Error(ExceptionType.DB_PATCH_USER_NOT_PATCHED);
 
   return data;
 }
